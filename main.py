a027fbd13a42b1792b51ff46336fe819a1ee31ad
SGKespace <55636018+SGKespace@users.noreply.github.com>
2023-02-05 21:51:03 +0400

    заменил 3 вызова headers на 1,  исправил форматирование

--- main.py
+++ main.py
@@ -14,36 +14,34 @@ def main():
     url = createParser().url
     load_dotenv()
     token = os.environ["BITLY_TOKEN"]
+    headers = {'Authorization': token}
     try:
-        if is_bitlink(token, url):
-            number_clicks = count_clicks(token, url)
+        if is_bitlink(headers, url):
+            number_clicks = count_clicks(headers, url)
             print(f"По вашей ссылке было {number_clicks} перехода(ов)")
         else:
-            short_link = shorten_link(token, url)
+            short_link = shorten_link(headers, url)
             print(short_link)
     except requests.exceptions.HTTPError:
-            print('Вы ввели неправильную ссылку или неверный токен.')
+        print('Вы ввели неправильную ссылку или неверный токен.')
     except requests.exceptions.ConnectionError:
         print('ConnectionError: Не могу подключиться к серверу.')
 
 
-def is_bitlink(token, url):
-    headers = {'Authorization': token}
+def is_bitlink(headers, url):
     request_url = f"https://api-ssl.bitly.com/v4/bitlinks/{urlparse(url).netloc  + urlparse(url).path}"
     response = requests.get(request_url, headers=headers)
     return response.ok
 
 
-def count_clicks(token, url):
-    headers = {'Authorization': token}
+def count_clicks(headers, url):
     params = (('unit', 'day'), ('units', '-1'),)
     request_url = f"https://api-ssl.bitly.com/v4/bitlinks/{urlparse(url).netloc  + urlparse(url).path}/clicks/summary"
     response = requests.get(request_url, headers=headers, params=params)
     return response.json()['total_clicks']
     
 
-def shorten_link(token, url):
-    headers = {'Authorization': token}
+def shorten_link(headers, url):
     request_body = {"long_url": url}
     response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, json=request_body)
     response.raise_for_status()
