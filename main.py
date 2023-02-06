2a4d1d353c6b3e31d4b250afef0cad1be31abc48
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-06 18:28:04 +0400

    Передаем в функции токен

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ def main():
         url = input('Введите ссылку: ')
     load_dotenv()
     token = os.environ["BITLY_TOKEN"]
-    headers = {'Authorization': token}
+
     try:
-        if is_bitlink(headers, url):
-            number_clicks = count_clicks(headers, url)
+        if is_bitlink(token, url):
+            number_clicks = count_clicks(token, url)
             print(f"По вашей ссылке было {number_clicks} перехода(ов)")
         else:
-            short_link = shorten_link(headers, url)
+            short_link = shorten_link(token, url)
             print(short_link)
     except requests.exceptions.HTTPError:
         print('Вы ввели неправильную ссылку или неверный токен.')
@@ -30,14 +30,16 @@ def main():
         print('ConnectionError: Не могу подключиться к серверу.')
 
 
-def is_bitlink(headers, url):
+def is_bitlink(token, url):
+    headers = {'Authorization': token}
     url_parse = urlparse(url)
     request_url = f"https://api-ssl.bitly.com/v4/bitlinks/{url_parse.netloc}{url_parse.path}"
     response = requests.get(request_url, headers=headers)
     return response.ok
 
 
-def count_clicks(headers, url):
+def count_clicks(token, url):
+    headers = {'Authorization': token}
     params = (('unit', 'day'), ('units', '-1'),)
     url_parse = urlparse(url)
     request_url = f"https://api-ssl.bitly.com/v4/bitlinks/{url_parse.netloc}{url_parse.path}/clicks/summary"
@@ -45,7 +47,8 @@ def count_clicks(headers, url):
     return response.json()['total_clicks']
     
 
-def shorten_link(headers, url):
+def shorten_link(token, url):
+    headers = {'Authorization': token}
     request_body = {"long_url": url}
     response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, json=request_body)
     response.raise_for_status()
